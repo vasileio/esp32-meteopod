76238bf5066cbf506fadb2d4c1d2f0af78cdb96c
Vasileios Bimpikas <10380438+vasileio@users.noreply.github.com>
2025-07-05 21:38:59 +0100

    moved uart init to separate component

--- main/main.c
+++ main/main.c
@@ -3,32 +3,7 @@
 
 #define TAG                  "MAIN"
 #define BLINK_GPIO           2
-#define UART_PORT            UART_NUM_1
-#define BUF_SIZE             1024
-#define UART_RX_PIN          16
-#define UART_TX_PIN          17
 #define WATCHDOG_TIMEOUT_SEC 5
-
-static void init_uart(app_ctx_t *ctx)
-{
-    uart_config_t cfg = {
-        .baud_rate = 115200,
-        .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
-    };
-    uart_param_config(UART_PORT, &cfg);
-    uart_set_pin(UART_PORT, UART_TX_PIN, UART_RX_PIN,
-                 UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-    /* commandQueue lives in ctx */
-    uart_driver_install(UART_PORT,
-                        BUF_SIZE * 2,
-                        BUF_SIZE * 2,
-                        20,
-                        &ctx->commandQueue,
-                        0);
-}
 
 static void watchdog_task(void *pvParameters)
 {
